fe9ee81855ee49565f36bcf36290373acc30f7b7
adam zethraeus <adam@zethrae.us>
2013-01-20 18:25:33 -0800

    browserVersionOf is clearner

--- libraries/browserVersionOf.js
+++ libraries/browserVersionOf.js
@@ -2,21 +2,16 @@ function browserVersionOf(unprefixedName, parentObject) {
 	if (typeof parentObject === "undefined") {
 		parentObject = window;
 	}
-	var msName = "ms" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1);
-	var mozName = "moz" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1);
-	var webkitName = "webkit" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1);
-	var oName = "o" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1);
-	if (typeof parentObject[unprefixedName] !== "undefined" && parentObject[unprefixedName] !== null) {
-		return parentObject[unprefixedName];
-	} else if (typeof parentObject[msName] !== "undefined" && parentObject[msName] !== null) {
-		return parentObject[msName];
-	} else if (typeof parentObject[mozName] !== "undefined" && parentObject[mozName] !== null) {
-		return parentObject[mozName];
-	} else if (typeof parentObject[webkitName] !== "undefined" && parentObject[webkitName] !== null) {
-		return parentObject[webkitName];
-	} else if (typeof parentObject[oName] !== "undefined" && parentObject[oName] !== null) {
-		return parentObject[oName]
-	} else {
-		throw "Browser lacks support for " + unprefixedName;
+	var browserVersions = [], i;
+	browserVersions.push(unprefixedName);
+	browserVersions.push("ms" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
+	browserVersions.push("moz" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
+	browserVersions.push("webkit" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
+	browserVersions.push("o" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
+	for (i = 0; i < browserVersions.length; i++) {
+		if (typeof parentObject[browserVersions[i]] !== "undefined" && parentObject[browserVersions[i]] !== null) {
+			return parentObject[browserVersions[i]];
+		}
 	}
+	throw "Browser lacks support for " + unprefixedName;
 }
