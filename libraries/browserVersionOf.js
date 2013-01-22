727df30fd25c43b3bc103f4c6b3b12c845a82c42
adam zethraeus <adam@zethrae.us>
2013-01-21 22:54:10 -0800

    more concise browserVersionOf

--- libraries/browserVersionOf.js
+++ libraries/browserVersionOf.js
@@ -1,7 +1,6 @@
 function browserVersionOf(unprefixedName, parentObject) {
-	if (typeof parentObject === "undefined") {
-		parentObject = window;
-	}
+	// parentObject is optional and usually omitted, set it to window if it's not specified.
+	parentObject = parentObject || window;
 	var browserVersions = [], i;
 	browserVersions.push(unprefixedName);
 	browserVersions.push("ms" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
@@ -9,9 +8,9 @@ function browserVersionOf(unprefixedName, parentObject) {
 	browserVersions.push("webkit" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
 	browserVersions.push("o" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
 	for (i = 0; i < browserVersions.length; i++) {
-		if (typeof parentObject[browserVersions[i]] !== "undefined" && parentObject[browserVersions[i]] !== null) {
+		if (typeof parentObject[browserVersions[i]] === "function") {
 			return parentObject[browserVersions[i]];
 		}
 	}
-	throw "Browser lacks support for " + unprefixedName;
+	throw "Browser lacks function: "+ unprefixedName;
 }
