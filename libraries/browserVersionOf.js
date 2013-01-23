bf9f14292691a5f4afeddad04c299e523fe4521a
adam zethraeus <adam@zethrae.us>
2013-01-23 01:01:09 -0800

    nicer yet browserVersionOf

--- libraries/browserVersionOf.js
+++ libraries/browserVersionOf.js
@@ -1,15 +1,16 @@
 function browserVersionOf(unprefixedName, parentObject) {
 	// parentObject is optional and usually omitted, set it to window if it's not specified.
 	parentObject = parentObject || window;
-	var browserVersions = [], i;
-	browserVersions.push(unprefixedName);
-	browserVersions.push("ms" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
-	browserVersions.push("moz" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
-	browserVersions.push("webkit" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
-	browserVersions.push("o" + unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1));
-	for (i = 0; i < browserVersions.length; i++) {
-		if (typeof parentObject[browserVersions[i]] === "function") {
-			return parentObject[browserVersions[i]];
+	var versions = [unprefixedName],
+		i,
+		suffix = unprefixedName.charAt(0).toUpperCase() + unprefixedName.substring(1);
+	versions.push("ms" + suffix);
+	versions.push("moz" + suffix);
+	versions.push("webkit" + suffix);
+	versions.push("o" + suffix);
+	for (i = 0; i < versions.length; i++) {
+		if (typeof parentObject[versions[i]] === "function") {
+			return parentObject[versions[i]];
 		}
 	}
 	throw "Browser lacks function: "+ unprefixedName;
